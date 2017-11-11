fbcf804a7e97db40968cc4d238429d68eee4f4b6
pkennedy89 <patrickken@gmail.com>
2017-11-11 22:32:20 +0000

    Finished logic for the app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Solar System Quiz
+
+A one page quiz app created in honour of Science Week 2017. Suitable for ages 8 to 80!
+
--- app/src/main/java/com/example/android/solarsystemquiz/MainActivity.java
+++ app/src/main/java/com/example/android/solarsystemquiz/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.solarsystemquiz;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +19,106 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void checkAnswers(View view){
+    public void submitAnswers(View view){
         int correctAnswers = 0;
 
-        RadioGroup questionOne = (RadioGroup) findViewById(R.id.question_one);
-        int id = questionOne.getCheckedRadioButtonId();
+        // Grab each of the correct answers
+
+        RadioButton questionOne = (RadioButton) findViewById(R.id.question_one_answer);
+        boolean questionOneCorrect = questionOne.isChecked();
+
+        RadioButton questionTwo = (RadioButton) findViewById(R.id.question_two_answer);
+        boolean questionTwoCorrect = questionTwo.isChecked();
+
+        CheckBox questionThreeOne = (CheckBox) findViewById(R.id.question_three_answer_one);
+        boolean questionThreeOneCorrect = questionThreeOne.isChecked();
+
+        CheckBox questionThreeTwo = (CheckBox) findViewById(R.id.question_three_answer_two);
+        boolean questionThreeTwoCorrect = questionThreeTwo.isChecked();
+
+        RadioButton questionFour = (RadioButton) findViewById(R.id.question_four_answer);
+        boolean questionFourCorrect = questionFour.isChecked();
+
+        RadioButton questionFive = (RadioButton) findViewById(R.id.question_five_answer);
+        boolean questionFiveCorrect = questionFive.isChecked();
+
+        RadioButton questionSix = (RadioButton) findViewById(R.id.question_six_answer);
+        boolean questionSixCorrect = questionSix.isChecked();
+
+        EditText questionSeven = (EditText) findViewById(R.id.question_seven_answer);
+        String questionSevenCorrect = questionSeven.getText().toString();
+
+        RadioButton questionEight = (RadioButton) findViewById(R.id.question_eight_answer);
+        boolean questionEightCorrect = questionEight.isChecked();
+
+        // Set up if statements
+
+        if (questionOneCorrect){
+            correctAnswers += 1;
+        }
+
+        if (questionTwoCorrect){
+            correctAnswers += 1;
+        }
+
+        if (questionThreeOneCorrect && questionThreeTwoCorrect){
+            correctAnswers += 1;
+        }
+
+        if (questionFourCorrect){
+            correctAnswers += 1;
+        }
+
+        if (questionFiveCorrect){
+            correctAnswers += 1;
+        }
+
+        if (questionSixCorrect){
+            correctAnswers += 1;
+        }
+
+        if(questionSevenCorrect.equals("Laika") || questionSevenCorrect.equals("laika") ||
+                questionSevenCorrect.equals("LAIKA") ){
+            correctAnswers += 1;
+        }
+
+        if (questionEightCorrect){
+            correctAnswers += 1;
+        }
+
+        displayMessage(correctAnswers);
+    }
+
+    public void displayMessage(int answers){
+        Context context = getApplicationContext();
+
+        if(answers == 1){
+            CharSequence text = "You got " + answers + " question correct. Study up and try again!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        if(answers <= 3){
+            CharSequence text = "You got " + answers + " questions correct. Study up and try again!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        if(answers >= 4 && answers <= 6){
+            CharSequence text = "You got " + answers + " questions correct. Good job!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+        if(answers > 7){
+            CharSequence text = "You got " + answers + " questions correct. You're out of this world!";
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
 
 
     }
